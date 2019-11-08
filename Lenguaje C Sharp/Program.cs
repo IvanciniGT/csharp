22afb87c27d95e0bed03154c85fe88bc266a83f8
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2019-11-08 07:21:36 +0100

    Primer cambio

--- Lenguaje C Sharp/Program.cs
+++ Lenguaje C Sharp/Program.cs
@@ -4,9 +4,19 @@ namespace Lenguaje_C_Sharp
 {
     class Program
     {
+        private const string mensajeInicial = "Hola mundo!";
+
+        public static string MensajeInicial
+        {
+            get
+            {
+                return mensajeInicial;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MensajeInicial);
         }
     }
 }
